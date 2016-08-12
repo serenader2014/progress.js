137297c28bdd5bcb4af5c8e34c4a1260fe9c1015
serenader <xyslive@gmail.com>
2016-08-12 18:00:21 +0800

    update script

---
+++ demo/demo.js
@@ -0,0 +1,6 @@
+import Progress from '../src/progress.js'
+import './style.css'
+
+const progress = new Progress({
+
+})
--- src/progress.js
+++ src/progress.js
@@ -1,1 +1,12 @@
 import './progress.css'
+
+const defaultOption = {
+  color: 'blue'
+}
+
+export default class Progress {
+  constructor(options) {
+    this.options = Object.assign({}, defaultOption, options)
+  }
+}
+
