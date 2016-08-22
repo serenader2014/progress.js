b73acea2d170a7b74f6187074f427326d86957be
serenader <xyslive@gmail.com>
2016-08-22 23:53:16 +0800

    update npm script

--- demo/demo.js
+++ demo/demo.js
@@ -1,25 +1,27 @@
-const stop = document.querySelector('button')
+window.onload = function () {
+    const stop = document.querySelector('button')
 
-const progress = new Progress({
+    const progress = new Progress({
 
-})
+    })
 
-function randomNumber(length) {
-  return Math.floor(Math.random() * length)
+    function randomNumber(length) {
+      return Math.floor(Math.random() * length)
+    }
+
+    progress.on('start', function() {
+      console.log('start')
+    })
+
+    progress.on('progress', function(p) {
+      console.log('progress', p)
+      // progress.setColor(`rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`)
+    })
+
+    progress.start()
+    console.log(progress)
+
+    stop.addEventListener('click', function() {
+      progress.end()
+    })
 }
-
-progress.on('start', () => {
-  console.log('start')
-})
-
-progress.on('progress', (p) => {
-  console.log('progress', p)
-  // progress.setColor(`rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`)
-})
-
-progress.start()
-console.log(progress)
-
-stop.addEventListener('click', () => {
-  progress.end()
-})
